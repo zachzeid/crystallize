6ba7018bf2ca4c0d7ead2299a58a25d4aa86fbb7
zach.zeid <zach.zeid@gmail.com>
2018-01-22 10:45:29 -0500

    Added more id's to button elements.  Bound button elements to .click functions in index.js.  Console log output shows what the function of the button is supposed to do.

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ $(document).ready(function() {
     console.log('saving skill');
 
   });
+  $('#delete-skill').click(function(){
+    console.log('deleting skill');
+  });
+
+  $('#main-skill-select').click(function(){
+    console.log('Adding skill')
+  });
+
+  $('#add-prereq-or').click(function(){
+    console.log('Adding pre-req from skill');
+  });
+
+  $('#del-prereq-or').click(function(){
+    console.log('Removing pre-req from skill');
+  });
+  $('#del-skill-prereq-and').click(function(){
+    console.log('Removing skill from pre-req list');
+  });
+  $('#add-skill-prereq-and').click(function(){
+    console.log('Adding skill to pre-req list');
+  });
 
 
 });
