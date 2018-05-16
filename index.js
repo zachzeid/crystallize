9033c8674a1c6fe18e9fe9e6ff592c14ee2eabf3
zach.zeid <zach.zeid@gmail.com>
2018-05-16 16:44:17 -0400

    This was so long ago I forgot what I did.

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-$(document).ready(function() {
-  $('#save-skill').click(function() {
+$(document).ready(function () {
+  $('#save-skill').click(function (event) {
     console.log('saving skill');
-
+    event.preventDefault();
   });
-  $('#delete-skill').click(function(){
+  $('#delete-skill').click(function (event) {
     console.log('deleting skill');
+    event.preventDefault();
   });
 
-  $('#main-skill-select').click(function(){
+  $('#main-skill-select').click(function (event) {
     console.log('Adding skill')
+    event.preventDefault();
   });
 
-  $('#add-prereq-or').click(function(){
+  $('#add-prereq-or').click(function (event) {
     console.log('Adding pre-req from skill');
+    event.preventDefault();
   });
 
-  $('#del-prereq-or').click(function(){
+  $('#del-prereq-or').click(function (event) {
     console.log('Removing pre-req from skill');
+    event.preventDefault();
   });
-  $('#del-skill-prereq-and').click(function(){
+  $('#del-skill-prereq-and').click(function (event) {
     console.log('Removing skill from pre-req list');
+    event.preventDefault();
   });
-  $('#add-skill-prereq-and').click(function(){
+  $('#add-skill-prereq-and').click(function (event) {
     console.log('Adding skill to pre-req list');
+    event.preventDefault();
   });
-  $('#import-json').click(function(){
+  $('#import-json').click(function (event) {
     console.log('Importing JSON into Crystallize.');
+    event.preventDefault();
   });
-  $('#export-json').click(function(){
+  $('#export-json').click(function (event) {
     console.log('Exporting JSON from Crystallize');
+    event.preventDefault();
   });
 
 
